5e6d5838f329eb560dc8318f0f3cb8c1bd3891c1
ManavKasare <60919769+ManavKasare@users.noreply.github.com>
2020-03-28 02:45:56 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'), 
+    url(r'^$', ListView.as_view(queryset = Post.objects.all().order_by('-created_on')[:25],
+        template_name = 'dashboard/dashboard.html')),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Post, template_name = 'dashboard/dashboard.html')),
+]
+
+
